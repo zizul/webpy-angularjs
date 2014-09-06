ececfb4d9c21699dec9aa5de9449aca7d7e74eaa
Kacper Zdanowicz <kac_@poczta.onet.pl>
2014-09-06 17:06:07 +0200

    css remove

--- js/main.js
+++ js/main.js
@@ -24,11 +24,11 @@ app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
 app.controller('PageCtrl', function (/* $scope, $location, $http */) {
 	console.log("Page Controller reporting for duty.");
 	// Activates the Carousel
-	$('.carousel').carousel({
-		interval: 5000
-	});
+	//$('.carousel').carousel({
+	//	interval: 5000
+	//});
 	// Activates Tooltips for Social Links
-	$('.tooltip-social').tooltip({
-		selector: "a[data-toggle=tooltip]"
-	})
+	//$('.tooltip-social').tooltip({
+	//	selector: "a[data-toggle=tooltip]"
+	//})
 });
